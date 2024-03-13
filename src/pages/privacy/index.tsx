f83283f0e0cfad94ccfa7d34c755c6ca4c1bd926
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2024-03-13 10:08:11 +0200

    feat: update Privacy Policy

--- src/pages/privacy/index.tsx
+++ src/pages/privacy/index.tsx
@@ -10,53 +10,120 @@ export default function Terms() {
     <main>
       <SimpleHeader
         title="Privacy Policy for Atlantes.io"
+        subtitle="Last Updated: January 2, 2024"
       />
       <PolicySection>
         <h2>Introduction:</h2>
         <p>
-          Atlantes.io (&quot;we&quot;, &quot;us&quot;, or &quot;our&quot;) is commiìed to protecting the privacy and security of your personal information. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our services and products.
-        </p>
-        <h2>1. Information Collection:</h2>
-        <p>
-          We collect information that you provide directly to us, such as when you create an account, use our services, or communicate with us. This may include personal information like your name, email address, and payment details. We also automatically collect certain information about your device and how you interact with our services.
-        </p>
-        <h2>2. Use of Information:</h2>
-        <p>
-          The information we collect is used to provide, maintain, and improve our services, to process transactions, to communicate with you, and to comply with legal obligations. We may also use the information for analytics purposes to enhance our services.
-        </p>
-        <h2>3. Sharing of Information:</h2>
-        <p>
-          We do not sell your personal information to third parties. We may share information with service providers who perform services on our behalf, with business partners, or if required by law. We also reserve the right to transfer your information in the event of a sale, merger, or reorganization of our company.
-        </p>
-        <h2>4. Data Security:</h2>
-        <p>
-          We implement a variety of security measures to maintain the safety of your personal information. However, no electronic transmission or storage of information can be entirely secure. While we strive to use commercially acceptable means to protect your personal information, we cannot guarantee its absolute security.
-        </p>
-        <h2>5. Your Rights:</h2>
-        <p>
-          Depending on your location, you may have certain rights regarding your personal information, including the right to access, correct, or delete your personal information we hold. To exercise these rights, please contact us.
-        </p>
-        <h2>6. International Transfers:</h2>
-        <p>
-          Your information may be transferred to, and maintained on, computers located outside of your state, province, country, or other governmental jurisdiction where the data protection laws may differ from those of your jurisdiction.
-        </p>
-        <h2>7. Children’s Privacy:</h2>
-        <p>
-          Our services are not intended for individuals under the age of 18. We do not knowingly collect personal information from children under 18. If we become aware that we have collected personal information from a child under 18, we will take steps to delete such information from our servers.
-        </p>
-        <h2>8. Changes to This Privacy Policy:</h2>
-        <p>
-          We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page and updating the &quot;effective date&quot; at the top of this Privacy Policy.
-        </p>
-        <h2>9. Contact Us:</h2>
-        <p>
-          If you have any questions about this Privacy Policy, please contact us via the support channels provided on our website.
+          At Adwirk GmbH, operating Atlantes.io, we prioritize your privacy and the security of your personal data (&quot;Personal Data&quot;). This Privacy Policy outlines how we collect, use, disclose, and protect your information across our platform, including our related services, functionalities, applications, and content. Your interaction with Atlantes.io signifies your agreement to this Privacy Policy. If you disagree with any part, please cease using our services immediately.
         </p>
 
-        {/* Effective Date: 01.11.2023 (Last Update)
+        <h2>Collection of Personal Data:</h2>
         <p>
-          This outline serves as a basic framework. Again, due to the intricacies of privacy laws and the specific operations of Atlantes.io, it&apos;s crucial to consult with legal professionals to ensure the Privacy Policy is compliant with all relevant laws and accurately reflects your data handling practices.
-        </p> */}
+          &quot;Personal Data&quot; refers to any information relating to an identifiable person. Depending on your interactions with us, this may include:
+        </p>
+        <ul>
+          <li>
+            Account and profile information (e.g., email, blockchain wallet addresses).
+          </li>
+          <li>
+            Transaction details, including payment information processed by thirdparty vendors.
+          </li>
+          <li>
+            Contact information from third-party services (e.g., Telegram, Discord).
+          </li>
+          <li>
+            Technical and usage data, such as IP addresses, device information, and browsing patterns, collected via server logs and cookies.
+          </li>
+          <li>
+            Marketing preferences and communication history.
+          </li>
+        </ul>
+        <p>
+          We may also collect information from public records, social media, and blockchain transactions associated with a wallet address.
+        </p>
+
+        <h2>Purposes for Processing Your Personal Data:</h2>
+        <p>
+          We process your Personal Data for purposes including:
+        </p>
+        <ul>
+          <li>
+            Delivering and personalizing our services.
+          </li>
+          <li>
+            Developing and improving our offerings.
+          </li>
+          <li>
+            Communicating with you about service updates and marketing messages.
+          </li>
+          <li>
+            Managing our business operations and complying with legal requirements.
+          </li>
+          <li>
+            Facilitating account registration and transactions.
+          </li>
+        </ul>
+
+        <h2>Disclosure of Personal Data:</h2>
+        <p>
+          Your Personal Data may be shared with:
+        </p>
+        <ul>
+          <li>
+            Service providers and vendors aiding our service delivery.
+          </li>
+          <li>
+            Potential business partners or parties involved in business asset transactions.
+          </li>
+          <li>
+            Government and law enforcement agencies, as required by law.
+          </li>
+          <li>
+            Any other entity with your express consent.
+          </li>
+        </ul>
+
+        <h2>Data Security:</h2>
+        <p>
+          We implement measures to protect your Personal Data but cannot guarantee absolute security against unauthorized access or cyber threats.
+        </p>
+
+        <h2>Children’s Privacy:</h2>
+        <p>
+          Atlantes.io is not intended for individuals under 18. We do not knowingly collect data from children without parental consent.
+        </p>
+
+        <h2>Cookies and Tracking:</h2>
+        <p>
+          We use cookies to enhance website functionality, personalize content, and analyze site traffic. You can control cookie settings through your browser.
+        </p>
+
+        <h2>Your Choices and Rights:</h2>
+        <p>
+          You have rights regarding your Personal Data, including access, correction, and deletion. Contact us to exercise these rights or to withdraw consent for data processing.
+        </p>
+
+        <h2>Non-Targeting of European Individuals:</h2>
+        <p>
+          We do not deliberately target our services at individuals in Europe and comply with relevant non-European regulations.
+        </p>
+
+        <h2>Changes to This Privacy Policy:</h2>
+        <p>
+          We reserve the right to update this policy and will notify you of significant changes. Regularly review this policy to stay informed.
+        </p>
+
+        <h2>Contacting Us:</h2>
+        <p>
+          For questions and concerns about our Privacy Policy or your Personal Data, contact&nbsp;
+          <a href="mailto:support@atlantes.io">support@atlantes.io</a>
+        </p>
+
+        <h2>Conclusion:</h2>
+        <p>
+          Your privacy is of utmost importance to us at Atlantes.io, developed by Adwirk GmbH. We are committed to safeguarding your information and enhancing yourexperience with our services.
+        </p>
 
       </PolicySection>
       <JoinUsSection />
