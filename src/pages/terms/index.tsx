015e41ee9af15cd483b1d9c8b93a3e33b9fd723f
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2024-03-13 09:35:49 +0200

    feat: update ToS

--- src/pages/terms/index.tsx
+++ src/pages/terms/index.tsx
@@ -10,66 +10,77 @@ export default function Terms() {
     <main>
       <SimpleHeader
         title="Terms of&nbsp;Service"
+        subtitle="Last updated: January 2, 2024"
       />
       <PolicySection>
-        <h2>Introduction:</h2>
         <p>
-          Welcome to Atlantes.io. By accessing or using our products and services, you agree to be bound by the following terms and conditions (the &quot;Terms&quot;). Please read them carefully. If you do not agree to these Terms, you must not use our services.
+          <b>Welcome to Atlantes.io</b>
+          , operated by Adwirk GmbH (&quot;we&quot;, &quot;us&quot;, &quot;our&quot;), with our registered office at&nbsp;Haunstetter Str. 112, 86161 Augsburg, Germany, Company ID - HRB 251372, VAT - DE327156808. Atlantes.io provides services and products related to analytics and copy trading in blockchain environments (&quot;Services&quot;). By accessing or using our Services, you agree to these Terms of Use (&quot;Terms&quot;), our Privacy Policy, and any other guidelines, rules, or policies to our,  incorporated herein by&nbsp;reference
+        </p>
+        <h2>1. Modifications to Services and&nbsp;Terms</h2>
+        <p>
+          We continuously evolve our Services to enhance user experience and comply with regulatory changes. As such, we reserve the right to modify these Terms and our Services without prior notice. Your continued use post-modification constitutes acceptance of the revised terms.
         </p>
 
-        <h2>1. Acceptance of Terms:</h2>
+        <h2>2. Account Registration and Use</h2>
         <p>
-          By using Atlantes.io, you confirm that you accept these Terms and that you agree to comply with them. These Terms apply to all users of our services.
+          To access certain features of Atlantes.io, you may need to register an account. You must provide accurate and current information. The account is for your personal use only and must not be shared. You are responsible for maintaining the confidentiality of your account information and all activities under your account. Notify us immediately if you suspect any unauthorized use of your account.
         </p>
 
-        <h2>2. Services Description:</h2>
+        <h2>3. Compliance with Laws</h2>
         <p>
-          Atlantes.io provides analytical and informational tools related to cryptocurrency trading. Our products, including but not limited to Trade Volume Bot, Wallet Analyzer, and Profit Maker, are designed to offer market insights and analytics. These tools are intended for informational purposes only and do not constitute financial advice.
+          You agree to use Atlantes.io in compliance with all applicable laws and regulations, especially those related to digital assets and securities. It&apm;s your responsibility to ensure that your use of our Services does not contravene laws applicable to you.
         </p>
 
-        <h2>3. No Financial Advice:</h2>
+        <h2>4. Intellectual Property</h2>
         <p>
-          The content provided by Atlantes.io is for informational purposes only and is not intended as financial, investment, tax, legal, or other types of advice. You should not rely on any information or services provided by us as a substitute for personal research or consultation with professional advisers.
+          All content, software, and intellectual property related to Atlantes.io are owned by Adwirk GmbH or its licensors. You are granted a limited, non-exclusive license to access and use our Services for your personal, non-commercial use, subject to these Terms.
         </p>
 
-        <h2>4. Disclaimer of Liability:</h2>
+        <h2>5. Governing Law and Dispute Resolution</h2>
         <p>
           Atlantes.io, its affiliates, and its service providers will not be liable for any loss or damage, whether direct, indirect, consequential, or otherwise, arising from your use of our services or reliance on the information provided. This includes, but is not limited to, loss of profit, goodwill, data, or other intangible losses.
         </p>
 
-        <h2>5. User Responsibilities:</h2>
+        <h2>6. Web3 Services</h2>
         <p>
-          You are responsible for all decisions and actions resulting from the use of our services. You agree to use Atlantes.io&apos;s products for lawful purposes only and in accordance with these Terms.
+          If you use our Services through Web3 technologies, including blockchain wallets, you agree to our collection and use of related data in accordance with our Privacy Policy. You are solely responsible for the security of your Digital Wallet and its credentials.
         </p>
 
-        <h2>6. Intellectual Property Rights:</h2>
+        <h2>7. Acceptable Use</h2>
         <p>
-          All intellectual property rights in the services, tools, and content provided by Atlantes.io are owned by or licensed to us and are protected by copyright and other laws. You may not use any content without our express written permission.
+          You must use Atlantes.io solely for lawful purposes and in a manner that does not infringe the rights of, restrict or inhibit anyone else's use and enjoyment of Atlantes.io. Prohibited activities include, but are not limited to, unauthorized copying, reproduction, or modification of any part of our Services.
         </p>
 
-        <h2>7. Amendments to Terms:</h2>
+        <h2>8. Disclaimer</h2>
         <p>
-          Atlantes.io reserves the right to modify these Terms at any time. Your continued use of our services following any changes indicates your acceptance of the new Terms.
+          Atlantes.io and its Services are provided on an &quot;as is&quot; basis without warranties of any kind. We do not offer investment advice, and our Services should not be construed as such. You bear sole responsibility for your investment and trading decisions.
         </p>
 
-        <h2>8. Governing Law:</h2>
+        <h2>9. Limitation of Liability</h2>
         <p>
-          These Terms shall be governed and construed in accordance with the laws of England and Wales, without regard to its conflict of law provisions.
+          Adwirk GmbH will not be liable for any indirect, incidental, special, or consequential damages arising out of or in connection with your use of Atlantes.io.
         </p>
 
-        <h2>9. Severability:</h2>
+        <h2>10. Indemnification</h2>
         <p>
-          If any provision of these Terms is held to be invalid or unenforceable, such provision shall be struck and the remaining provisions shall be enforced.
+          You agree to indemnify Adwirk GmbH against any claims, damages, or costs arising from your use of Atlantes.io in violation of these Terms.
         </p>
 
-        <h2>10. Contact Us:</h2>
+        <h2>11. Termination</h2>
         <p>
-          For any questions or concerns regarding these Terms or our services, please contact us via the support channels provided on our website.
+          We may terminate or suspend your access to Atlantes.io at any time, without notice, for any reason, including violation of these Terms.
         </p>
 
-        <h2>Conclusion:</h2>
+        <h2>12. Miscellaneous</h2>
         <p>
-          By accessing and using Atlantes.io&apos;s services, you acknowledge that you have read, understood, and agreed to be bound by these Terms of Use.
+          These Terms constitute the entire agreement between you and Adwirk GmbH regarding your use of Atlantes.io. If any part of these Terms is found to be unenforceable, the remaining terms will remain in effect.
+        </p>
+
+        <h2>Contact Us</h2>
+        <p>
+          For any questions regarding these Terms, please contact us at&nbsp;
+          <a href="mailto:support@atlantes.io">support@atlantes.io</a>
         </p>
       </PolicySection>
       <JoinUsSection />
