0e9c310af146b16c4878529114962496ff87300a
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2024-03-15 08:57:55 +0200

    refactor: udpate pagination to use svg bullets

--- src/components/SliderPagination.tsx
+++ src/components/SliderPagination.tsx
@@ -16,13 +16,24 @@ export default function SliderPagination(props: SliderPaginationProps) {
   return (
     <div className={styles.pagination}>
       {Array.from({ length: slidesCount }, (_, index) => (
-        <span
+        // <span
+        //   className={clsx({
+        //     [styles.bullet]: true,
+        //     [styles.currentBullet]: currentIndex === index,
+        //   })}
+        //   key={index}
+        // />
+        <svg
           className={clsx({
             [styles.bullet]: true,
             [styles.currentBullet]: currentIndex === index,
           })}
           key={index}
-        />
+          viewBox="0 0 100 100"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle r="45" cx="50" cy="50" />
+        </svg>
       ))}
     </div>
   );
